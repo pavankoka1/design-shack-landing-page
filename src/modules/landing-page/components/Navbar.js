2800f08b6588b4befcfb2e6b6c58cbd1f2a830ad
pavan koka <pavan.koka@toppr.com>
2022-06-01 22:15:09 +0530

    changes

--- src/modules/landing-page/components/Navbar.js
+++ src/modules/landing-page/components/Navbar.js
@@ -1,23 +1,189 @@
-import React from 'react'
-import SIGN_UP from 'assets/icons/sign-up.svg'
-import DESIGN_SHACK from 'assets/logos/design-shack.svg';
-import styles from './Navbar.module.scss';
+// import React from 'react'
+// import AppBar from '@mui/material/AppBar';
+// import Box from '@mui/material/Box';
+// import Toolbar from '@mui/material/Toolbar';
+// import SIGN_UP from 'assets/icons/sign-up.svg'
+import DESIGN_SHACK from "assets/logos/design-shack.svg";
+import styles from "./Navbar.module.scss";
 
-function Navbar() {
-  return (
-    <div className={styles.wrapper}>
-        <img src={DESIGN_SHACK} alt='logo' />
-        <div className={styles.links}>
-          <p>About</p>
-          <p>Create Now</p>
-          <p>Gallery</p>
-          <div className={styles.signUp}>
-            <img src={SIGN_UP} alt='sign up' />
-            <p>Sign Up</p>
-          </div>
-        </div>
-    </div>
-  )
-}
+// function Navbar() {
+//   return (
+//     <div className={styles.wrapper}>
+//         {/* <img src={DESIGN_SHACK} alt='logo' />
+//         <div className={styles.links}>
+//           <p>About</p>
+//           <p>Create Now</p>
+//           <p>Gallery</p>
+//           <div className={styles.signUp}>
+//             <img src={SIGN_UP} alt='sign up' />
+//             <p>Sign Up</p>
+//           </div>
+//         </div> */}
+//         <AppBar position="fixed">
+//           <Toolbar>koka</Toolbar>
+//         </AppBar>
+//         <Toolbar />
+//     </div>
+//   )
+// }
 
-export default Navbar
+// export default Navbar
+
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import AdbIcon from "@mui/icons-material/Adb";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const pages = ["About", "Create Now", "Gallery", "Sign Up"];
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
+
+const darkTheme = createTheme({
+    palette: {
+        mode: "light",
+        primary: {
+            main: "#fff",
+        },
+    },
+});
+
+const ResponsiveAppBar = () => {
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
+    return (
+        <ThemeProvider theme={darkTheme}>
+            <AppBar position="sticky" color="primary">
+                <Container maxWidth="xl">
+                    <Toolbar disableGutters sx={{justifyContent: 'space-between'}}>
+                        {/* <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} /> */}
+                        <Typography
+                            variant="h6"
+                            noWrap
+                            component="a"
+                            href="/"
+                            sx={{
+                                mr: 2,
+                                display: { xs: "none", md: "flex" },
+                                fontFamily: "monospace",
+                                fontWeight: 700,
+                                letterSpacing: ".3rem",
+                                color: "inherit",
+                                textDecoration: "none",
+                            }}
+                        >
+                            <img src={DESIGN_SHACK} alt="logo" />
+                        </Typography>
+
+                        <Box
+                            sx={{
+                                flexGrow: 1,
+                                display: { xs: "flex", md: "none" },
+                            }}
+                        >
+                            <IconButton
+                                size="large"
+                                aria-label="account of current user"
+                                aria-controls="menu-appbar"
+                                aria-haspopup="true"
+                                onClick={handleOpenNavMenu}
+                                color="inherit"
+                            >
+                                <MenuIcon />
+                            </IconButton>
+                            <Menu
+                                id="menu-appbar"
+                                anchorEl={anchorElNav}
+                                anchorOrigin={{
+                                    vertical: "bottom",
+                                    horizontal: "right",
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: "top",
+                                    horizontal: "right",
+                                }}
+                                open={anchorElNav ? true : false}
+                                onClose={handleCloseNavMenu}
+                                sx={{
+                                    display: { xs: "block", md: "none" },
+                                }}
+                            >
+                                {pages.map((page) => (
+                                    <MenuItem
+                                        key={page}
+                                        onClick={handleCloseNavMenu}
+                                    >
+                                        <Typography textAlign="center">
+                                            {page}
+                                        </Typography>
+                                    </MenuItem>
+                                ))}
+                            </Menu>
+                        </Box>
+                        <Typography
+                            variant="h5"
+                            noWrap
+                            component="a"
+                            href=""
+                            sx={{
+                                mr: 2,
+                                display: { xs: "flex", md: "none" },
+                                flexGrow: 1,
+                                fontFamily: "monospace",
+                                fontWeight: 700,
+                                letterSpacing: ".3rem",
+                                color: "inherit",
+                                textDecoration: "none",
+                            }}
+                        >
+                            <img src={DESIGN_SHACK} alt="logo" />
+                        </Typography>
+                        <Box sx={{ display: { xs: "none", md: "flex" } }}>
+                            {pages.map((page) => (
+                                <Button
+                                    key={page}
+                                    onClick={handleCloseNavMenu}
+                                    sx={{
+                                        my: 2,
+                                        color: "black",
+                                        display: "block",
+                                        marginLeft: "auto",
+                                    }}
+                                >
+                                    {page}
+                                </Button>
+                            ))}
+                        </Box>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+        </ThemeProvider>
+    );
+};
+export default ResponsiveAppBar;
--- src/modules/landing-page/components/first-fold.js
+++ src/modules/landing-page/components/first-fold.js
@@ -1,16 +1,128 @@
-import React from 'react'
-import RIGHT_IMAGE from 'assets/images/first-fold-right.svg'
+import React, {useState, useEffect} from 'react'
+import { motion } from "framer-motion"
+import Typical from 'react-typical'
+import IMAGE_1 from 'assets/images/first-fold-1.svg'
+import IMAGE_2 from 'assets/images/first-fold-2.svg'
+import IMAGE_3 from 'assets/images/first-fold-3.svg'
+import IMAGE_4 from 'assets/images/first-fold-4.svg'
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
 import styles from './first-fold.module.scss'
 
+const boxShadowValue = 'rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px';
+
 function FirstFold() {
+  const [activeImage, setActiveImage] = useState(0);
+
+  useEffect(() => {
+    var i = 0;
+    setInterval(() => {
+      i = i + 1;
+      setActiveImage(act => {
+        return (i % 4)
+      })
+    }, 5000);
+  }, [])
+
+  function renderAnimatedText() {
+    switch(activeImage) {
+      case 0:
+        return <Typical
+                  steps={[
+                    'New York skyline in Cubist Style', 5000
+                  ]}
+                />
+      case 1:
+        return <Typical
+                  steps={[
+                    'Volcano in oil painting', 5000,
+                  ]}
+                />
+      case 2:
+        return <Typical
+                  steps={[
+                    'Mushroom in watercolor painting', 5000,
+                  ]}
+                />
+      case 3:
+        return <Typical
+                  steps={[
+                    'Tree in pencil sketching', 5000,
+                  ]}
+                />
+    }
+  }
+
   return (
     <div className={styles.wrapper}>
-        <img className={styles.rightImage} src={RIGHT_IMAGE} alt='right' />
         <div className={styles.content}>
             <p className={styles.heading}>Instant New Designs.<br /> No Photoshop. Copyright Free</p>
             <p className={styles.description}>“Our powerful AI models generate novel designs by just using text and images”</p>
-            <p className={styles.bgLine}>New York skyline in Cubist Style</p>
-            <p className={styles.tryNow}>Try Now</p>
+            <p className={styles.bgLine}>
+              {renderAnimatedText()}
+            </p>
+            <Button size='large' color='error' variant='outlined' children='Create Now' />
+        </div>
+        <div style={{display: 'flex'}}>
+          <div style={{marginRight: '16px', width: '218px'}}>
+            <motion.img 
+              src={IMAGE_1} 
+              alt='first-fold' 
+              width={218} 
+              style={{
+                marginBottom: '16px',
+                boxShadow: activeImage === 0 ? boxShadowValue : '',
+                zIndex: activeImage === 0 ? 10 : 1,
+              }}
+              animate={{
+                scale: activeImage === 0 ? 1.2 : 1,
+              }}
+              transition={{ duration: 2 }}
+            />
+            <motion.img 
+              src={IMAGE_3} 
+              alt='first-fold' 
+              width={218}
+              style={{
+                boxShadow: activeImage === 1 ? boxShadowValue : '',
+                zIndex: activeImage === 1 ? 10 : 1,
+              }}
+              animate={{
+                scale: activeImage === 1 ? 1.2 : 1,
+              }}
+              transition={{ duration: 2 }}
+            />
+          </div>
+          <div>
+            <motion.img 
+              src={IMAGE_2} 
+              alt='first-fold' 
+              width={189} 
+              style={{
+                marginBottom: '16px', 
+                marginTop: '72px', 
+                boxShadow: activeImage === 2 ? boxShadowValue : '',
+                zIndex: activeImage === 2 ? 10 : 1,
+              }}
+              animate={{
+                scale: activeImage === 2 ? 1.2 : 1,
+              }}
+              transition={{ duration: 2 }} 
+            />
+            <motion.img 
+              src={IMAGE_4} 
+              alt='first-fold' 
+              width={218} 
+              style={{
+                boxShadow: activeImage === 3 ? boxShadowValue : '',
+                zIndex: activeImage === 3 ? 10 : 1,
+              }}
+              animate={{
+                scale: activeImage === 3 ? 1.2 : 1,
+              }}
+              transition={{ duration: 2 }}
+            />
+          </div>
         </div>
     </div>
   )
